0333f96fe7af7c9a992a071b20e3f8507d06c73d
ushi <>
2025-11-08 11:41:03 +0900

    feature: TodoListPage

---
+++ lib/presentation/view/todo_list_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  const TodoListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Todo List')),
+      body: const Center(child: Text('Todo List')),
+    );
+  }
+}
