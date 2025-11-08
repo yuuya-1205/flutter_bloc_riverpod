b428ca78b73726d7eb5130a6feb39372b475d7d3
ushi <>
2025-11-08 12:11:23 +0900

    feature: todo_list_page.dart

--- lib/presentation/view/todo_list_page.dart
+++ lib/presentation/view/todo_list_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_riverpod/data/repository/todo_repository.dart';
+import 'package:flutter_bloc_riverpod/domain/bloc/todo_bloc.dart';
 
+/// BlocのテストとRiverpodのテストを書いてみるかな。
 class TodoListPage extends StatelessWidget {
   const TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Todo List')),
-      body: const Center(child: Text('Todo List')),
+    return BlocProvider(
+      create: (context) => TodoBloc(context.read<TodoRepository>()),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Todo List')),
+        body: const Center(child: Text('Todo List')),
+      ),
     );
   }
 }
