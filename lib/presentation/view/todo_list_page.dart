c6fa783f63a48f3f7d0d97561b261cdc8fef0c03
ushi <>
2025-11-10 03:09:59 +0900

    feature: bloc_list_page

--- lib/presentation/view/todo_list_page.dart
+++ lib/presentation/view/todo_list_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_riverpod/data/repository/todo_repository.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_riverpod/domain/bloc/todo_bloc.dart';
+import 'package:flutter_bloc_riverpod/domain/bloc/todo_event.dart';
+import 'package:flutter_bloc_riverpod/domain/model/todo.dart';
 
 class TodoListPage extends StatelessWidget {
   const TodoListPage({super.key});
@@ -9,26 +11,32 @@ class TodoListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Todo List')),
-      body: Center(
-        child: Consumer(
-          builder: (context, ref, child) {
-            return StreamBuilder(
-              stream: ref.watch(todoRepositoryProvider).fetchTodos(),
-              builder: (context, snapshot) {
-                return Column(
-                  children: [
-                    Text('${snapshot.data?.length} todos'),
-                    ElevatedButton(
-                      onPressed: () =>
-                          ref.read(todoRepositoryProvider).addTodo('test'),
-                      child: const Text('Add Todo'),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-        ),
+      body: BlocBuilder<TodoBloc, List<Todo>>(
+        builder: (context, state) {
+          return Column(
+            children: [
+              StreamBuilder<List<Todo>>(
+                stream: context.read<TodoBloc>().todoStream(),
+                builder: (context, snapshot) {
+                  final todos = snapshot.data ?? [];
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: todos.length,
+                    itemBuilder: (context, index) {
+                      return Text(snapshot.data?[index].title ?? '');
+                    },
+                  );
+                },
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<TodoBloc>().add(AddTodoEvent('こんばんは'));
+                },
+                child: const Text('add Todo'),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
